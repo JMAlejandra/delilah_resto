e13633bf063565a666fb2aa976633f24631a5813
JMAlejandra <j.maria.alejandra@gmail.com>
2020-09-12 02:43:19 -0300

    T08 - Created users router and added the reference to server.js

---
+++ routes/users.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const router = express.Router()
+const sql = require('../mysql')
+const queries = require('../sql/queries')
+
+// CREATING NEW USER
+
+// GETTING ALL USERS
+
+// GETTING A USER BY ID
+
+// UPDATING A USER BY ID
+
+// DELETING A USER BY ID
+
+// LOGGING A USER BY ID
+
+
+module.exports = router
