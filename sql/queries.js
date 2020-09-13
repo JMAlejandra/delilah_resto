f26319709ef9c7f312e231726518f29a2d95adc2
JMAlejandra <j.maria.alejandra@gmail.com>
2020-09-12 21:12:55 -0300

    T15 - Created users/:id route and added the corresponding query to queries.js file

--- sql/queries.js
+++ sql/queries.js
@@ -1,5 +1,7 @@
 const getAllUsers = 'Select id, full_name, username, email, phone, address, created_at, is_admin from users'
+const getUserById = `Select id, full_name, username, email, phone, address, created_at, is_admin from users where id = `
 
 module.exports = {
     getAllUsers,
+    getUserById
 }
