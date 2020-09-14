f40080a84fe1219ae88a0d8089f3849fd073d83e
JMAlejandra <j.maria.alejandra@gmail.com>
2020-09-13 23:28:21 -0300

    T28 - Created verifyUserToken middleware to check request token

---
+++ middlewares/verifyUserToken.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+
+const jwt = require('jsonwebtoken')
+
+const verifyUserToken = async (req, res, next) => {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) {
+        return res.status(401).json({ error: "Expected user authentication token" })
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.status(403).send({ message: "User does not have access to the resource" })
+        res.locals.adminUser = user
+        next()
+    })
+}
+
+module.exports = verifyUserToken
