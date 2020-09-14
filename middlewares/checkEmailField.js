ae2283102684f97d3176d1ec0b48c0025333904f
JMAlejandra <j.maria.alejandra@gmail.com>
2020-09-13 21:46:58 -0300

    T19.1 - Abstacted checkEmailFormat from checkEmailField middleware and added it to utils folder

--- middlewares/checkEmailField.js
+++ middlewares/checkEmailField.js
@@ -1,11 +1,9 @@
-const checkEmailFormat = (email) => {
-    const reg = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return reg.test(email);
-}
+const checkEmailFormat = require("../utils/checkEmailFormat")
 
 const checkEmailField = (req, res, next) => {
     const { email } = req.body;
     if (checkEmailFormat(email)) {
+        console.log("test")
         return next()
     } else {
         res.status(400).send("Sent email address is not an email")
