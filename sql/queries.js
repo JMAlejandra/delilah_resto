63368dc4454e6b4711d827d505381b105b307b19
JMAlejandra <j.maria.alejandra@gmail.com>
2020-09-13 21:50:08 -0300

    T19.2 - Created queries to fetch user credentials by username or email

--- sql/queries.js
+++ sql/queries.js
@@ -5,12 +5,14 @@ const createNewUser = `INSERT INTO users
                         VALUES 
                         (:full_name, :username, :email, :phone, :address, :user_password, :is_admin);`
 const userIsAdminById = `select is_admin from users where id = :id`
-const getUserCredentials = `select id, is_admin, username, user_password from users where username = :username`
+const getUserCredentialsByUsername = `select id, is_admin, username, user_password from users where username = :username`
+const getUserCredentialsByEmail = `select id, is_admin, username, user_password, email from users where email = :email`
 
 module.exports = {
     getAllUsers,
     getUserById,
     createNewUser,
     userIsAdminById,
-    getUserCredentials,
+    getUserCredentialsByUsername,
+    getUserCredentialsByEmail,
 }
