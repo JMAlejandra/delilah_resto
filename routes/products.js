81a4ab6c679eaa16e40ee53d9adc288281ac5a09
JMAlejandra <j.maria.alejandra@gmail.com>
2020-09-18 02:57:24 -0300

    T22/T24 - Created POST/products/ and PUT/products/:id routes

--- routes/products.js
+++ routes/products.js
@@ -7,6 +7,7 @@ const jwt = require('jsonwebtoken')
 // MIDDLEWARES
 const verifyUserToken = require("../middlewares/verifyUserToken")
 const isUserAdmin = require("../middlewares/isUserAdmin")
+const checkNewProductFields = require("../middlewares/checkNewProductsFields")
 
 // GETTING ALL PRODUCTS
 router.get('/', verifyUserToken, async (req, res) => {
@@ -37,13 +38,11 @@ router.get('/:id', verifyUserToken, async (req, res) => {
     try {
         let data = []
         if (parseInt(res.locals.user.is_admin) === 1) {
-            console.log("admin")
             data = await sql.query(queries.getProductById, {
                 type: sql.QueryTypes.SELECT,
                 replacements: { id: parseInt(req.params.id) }
             })
         } else {
-            console.log("no admin")
             data = await sql.query(queries.getEnabledProductById, {
                 type: sql.QueryTypes.SELECT,
                 replacements: { id: parseInt(req.params.id) }
@@ -59,8 +58,43 @@ router.get('/:id', verifyUserToken, async (req, res) => {
 })
 
 // CREATING A NEW PRODUCT - ADMIN ONLY
+router.post('/', verifyUserToken, isUserAdmin, checkNewProductFields, async (req, res) => {
+    try {
+        const { description, price, image_url, is_favorite, is_enabled } = req.body
+        const data = await sql.query(queries.createNewProduct, {
+            replacements: {
+                description, image_url,
+                price: parseFloat(price),
+                is_favorite: parseInt(is_favorite),
+                is_enabled: parseInt(is_enabled)
+            }
+        })
+        if (data[1] === 0) return res.status(404).json({ error: "Product not created" })
+        res.status(200).json({ message: "New product created successfully", id_product: data[0] })
+    } catch (err) {
+        res.status(500).send(`Database Error: ${err.message}`)
+    }
+})
 
 // UPDATING A PRODUCT BY ID - ADMIN ONLY
+router.put('/:id', verifyUserToken, isUserAdmin, checkNewProductFields, async (req, res) => {
+    try {
+        const { description, price, image_url, is_favorite, is_enabled } = req.body
+        const id = req.params.id
+        const data = await sql.query(queries.updateProductById, {
+            replacements: {
+                id, description, image_url,
+                price: parseFloat(price),
+                is_favorite: parseInt(is_favorite),
+                is_enabled: parseInt(is_enabled)
+            }
+        })
+        if (data[0].affectedRows === 0) return res.status(404).json({ Message: "Product not updated, no rows were affected" })
+        res.status(200).json({ message: "Product updated successfully" })
+    } catch (err) {
+        res.status(500).send(`Database Error: ${err.message}`)
+    }
+})
 
 // DELETING A PRODUCT BY ID - ADMIN ONLY
 router.delete('/:id', verifyUserToken, isUserAdmin, async (req, res) => {
@@ -70,7 +104,6 @@ router.delete('/:id', verifyUserToken, isUserAdmin, async (req, res) => {
                 replacements: { id: parseInt(req.params.id) }
             })
         if (data[0].affectedRows === 0) {
-            console.log(data)
             return res.status(404).json({ error: "Product ID not found" })
         }
         res.status(200).json({ message: "Product deleted successfully" })
