3f07cee03747164eaaca3de9c423a27bcbd32a39
JMAlejandra <j.maria.alejandra@gmail.com>
2020-09-25 00:49:55 -0300

    T11 - Created PUT /users/:id route

--- sql/queries.js
+++ sql/queries.js
@@ -9,6 +9,7 @@ const userIsAdminById = `select is_admin from users where id = :id`
 const getUserCredentialsByUsername = `select id, is_admin, username, user_password from users where username = :username`
 const getUserCredentialsByEmail = `select id, is_admin, username, user_password, email from users where email = :email`
 const updateUserRole = `UPDATE users SET is_admin = :is_admin WHERE id = :id ;`
+const updateUserById = `UPDATE users SET full_name = :full_name, username = :username, email = :email, phone = :phone, address = :address, user_password = :user_password WHERE id = :id;`
 
 // PRODUCT QUERIES
 const getAllProducts = `select * from products`
@@ -30,11 +31,9 @@ const updateProductById = `UPDATE products
                             WHERE id = :id `
 const updateIsFavoriteProductById = `UPDATE products SET is_favorite = :is_favorite WHERE id = :id`
 const updateIsEnabledProductById = `UPDATE products SET is_enabled = :is_enabled WHERE id = :id`
-//const getOrdersBoard = `CALL GetOrdersBoard();`
-//const getOrderDetailsById = `CALL GetOrderDetailsById(:order_id);`
-//const getUserOrderDetailsById = `CALL GetUserOrderDetailsById(:order_id , :user_id);`
-//const getOrderInfoById = `CALL GetOrderInfoById(:order_id);`
 const getProductPrice = `SELECT id as id_product, price from products where id = :id`
+
+// ORDERS
 const addNewProductToOrder = `INSERT INTO products_by_order (id, id_order, id_product, product_quantity, product_price) 
 VALUES (NULL, :id_order, :id_product, :quantity, :price)`
 const createNewOrder = `INSERT INTO orders (id, id_status, id_payment_option, id_user, total) 
@@ -120,4 +119,5 @@ module.exports = {
   getListOfOrderStatus,
   deleteOrderProductsById,
   deleteOrderById,
+  updateUserById
 }
